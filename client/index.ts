68b5bedc2d51b0e809939b67c7db90255dd5d20f
fan xia <gonorth@qq.com>
2023-06-20 01:01:37 +0800

    统一 client call 的方式

--- client/index.ts
+++ client/index.ts
@@ -12,21 +12,31 @@ export default function Connect({ uri, token }: { uri: string, token: string }) 
     const socket = io(uri, { auth: { type: "client", token: token } });
     const send = newSend(socket)
 
-    const call = async <T>(name: string, input?: I, target: string[] = [], msg_id = nanoid()) => {
+    // todo 类型和出入参规范化
+    const call = async <T>(name: string, input?: I, target: string[] = [], parser = async (msg) => Promise<T>, msg_id = nanoid()) => {
+        // 订阅本 id 的信息流
         const subscription = filter((v) => v.id === msg_id, msgChannel[Symbol.asyncIterator]())
-        const sockets = await pods()
-
-        // todo 先接受一个答案的长度值
-        if (sockets.length === 0) {
-            return concat([]) as unknown as AsyncIterableIterator<T>
-        }
-
         await send({
             id: msg_id,
             type: "call",
             data: { func: name, input, target }
         })
-        return take(sockets.length, subscription)
+        // 获取第一个消息
+        const first_msg = (await subscription.next()).value
+        if (first_msg.type === "return" && first_msg.data.status === "success") {
+            // 获取返回的长度
+            const len: number = first_msg.data.output["sockets"].length
+            if (len === 0) {
+                // 没有就返回空
+                return concat([]) as unknown as AsyncIterableIterator<ClientHook>
+            } else {
+                // 返回数据
+                return transform(len, parser, take(len, concat([first_msg], subscription)))
+            }
+        } else {
+            console.log(JSON.stringify(first_msg))
+            throw new Error("数据错误",)
+        }
     }
 
     // 获取所有 pod 的 id
@@ -41,37 +51,13 @@ export default function Connect({ uri, token }: { uri: string, token: string }) 
     }
 
     // 获取所有 pod 的可调用函数元信息
-    const meta = async (msg_id = nanoid()): Promise<AsyncIterableIterator<ClientHook>> => {
-        // 订阅本 id 的信息流
-        const subscription = filter((v) => v.id === msg_id, msgChannel[Symbol.asyncIterator]())
-        await send({
-            id: msg_id,
-            type: "call",
-            data: { func: "__meta__" }
+    const meta = () => call<ClientHook>("__meta__", undefined, [], async (msg) => {
+        return ({
+            ...(msg as any).data.output.msg.data.output,
+            id: (msg as any).data.output.msg.socketId,
+            hooks: await parseZodObjectFunc((msg as any).data.output.msg.data.output.hooks)
         })
-        // 获取第一个消息
-        const first_msg = (await collect(take(1, subscription)))[0]
-        if (first_msg.type === "return" && first_msg.data.status === "success") {
-            // 获取返回的长度
-            const len: number = first_msg.data.output["sockets"].length
-            if (len === 0) {
-                // 没有就返回空
-                return concat([]) as unknown as AsyncIterableIterator<ClientHook>
-            } else {
-                // 返回数据
-                return transform(len, async (msg) => {
-                    return ({
-                        ...(msg as any).data.output.msg.data.output,
-                        id: (msg as any).data.output.msg.socketId,
-                        hooks: await parseZodObjectFunc((msg as any).data.output.msg.data.output.hooks)
-                    })
-                }, take(len, concat([first_msg], subscription)))
-            }
-        } else {
-            console.log(JSON.stringify(first_msg))
-            throw new Error("数据错误",)
-        }
-    }
+    })
 
     socket.on("msg", async (data) => msgChannel.push(await allType.parseAsync(data)))
     return { meta, pods, call }
